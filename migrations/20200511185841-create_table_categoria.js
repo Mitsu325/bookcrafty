facbff739f9d2268b4a4aac916cd71f95939b509
Mitsu325 <patricia.mashiba@gmail.com>
2020-05-11 16:16:58 -0300

    Retirado os comentários

--- migrations/20200511185841-create_table_categoria.js
+++ migrations/20200511185841-create_table_categoria.js
@@ -2,14 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-
     return queryInterface.createTable(
       'categoria', 
       { 
@@ -24,20 +16,9 @@ module.exports = {
           allowNull: false
         } 
       });
-    
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-
-    
     return queryInterface.dropTable('categoria');
-
   }
 };
